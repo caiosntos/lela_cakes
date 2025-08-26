6652860c4bc1c982331a94ecd81ba2d753ddb3f2
caiosntos <caiogiovanists@outlook.com>
2025-08-26 19:31:19 -0300

    Implementando mais recursos frontend

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 5000;
 
 app.use(express.static(path.join(__dirname)));
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Olá do Node.js 🚀" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+app.listen(5000, () => {
+  console.log(`Servidor rodando em http://localhost:${5000}`);
 });
