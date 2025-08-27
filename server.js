5a43293e03184691b13ebc717f459845d678aaa8
caiosntos <caiogiovanists@outlook.com>
2025-08-27 13:58:09 -0300

    Finalizando funcionalidades da API

--- server.js
+++ server.js
@@ -43,16 +43,6 @@ app.put('/usuarios/:id', async (req, res) => {
   res.status(201).json(req.body);
 });
 
-
-app.delete('/usuariosq:id', async (req, res) =>{
-  await prisma.user.delete({
-    where:{
-      id: req.params.id
-    }
-  })
-  res.status(200).json({message:'Usuario deletado com sucesso!'});
-})
-
 app.use(express.static(path.join(__dirname)));
 app.get("/api/hello", (req, res) => {
 });
