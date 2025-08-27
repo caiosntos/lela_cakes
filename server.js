f62b0d17b9270b7286b0ba2e525e603fb200e994
caiosntos <caiogiovanists@outlook.com>
2025-08-26 21:42:08 -0300

    Inicializando conexão com banco de dados e rotas API para login dos usuarios

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
 const express = require("express");
 const path = require("path");
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 const app = express();
+app.use(express.json());
+
+
+app.post('/usuarios', async (req, res) => {  
+  await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+      address: req.body.address
+    }
+  });
+
+  res.status(201).json(req.body);
+});
+
+app.get('/usuarios', async (req, res) => {
+
+  const users = await prisma.user.findMany()
+  res.status(200).json(users);
+});
+
+app.put('/usuarios/:id', async (req, res) => {  
+  await prisma.user.update({
+    where:{
+      id: req.params.id
+    },
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+      address: req.body.address
+    }
+  });
+
+  res.status(201).json(req.body);
+});
 
 app.use(express.static(path.join(__dirname)));
 app.get("/api/hello", (req, res) => {
